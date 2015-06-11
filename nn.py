24002777f83b48546de1a3e19bbe5a1764964bdb
tahiry <trazafin@gmail.com>
2015-06-11 18:05:26 +0200

    ]

--- nn.py
+++ nn.py
@@ -6,10 +6,10 @@ import random
 # Learning rate:
 # Lower  = slower
 # Higher = less precise
-rate=.2
+rate=.1
 activation=0
 # Create random weights
-inWeight=[1, 1, .5]
+inWeight=[random.uniform(0, 1), random.uniform(0, 1), 1]
 #inWeight=[random.uniform(0, 1), random.uniform(0, 1), .5]
 
 # Start neuron with no stimuli
@@ -22,7 +22,7 @@ test+=[[1.0, 0.0, 1.0]]
 test+=[[1.0, 1.0, 1.0]]
 
 # Calculate response from neural input
-def outNeuron(midThresh):
+def outNeuron():
         global inNeuron, inWeight, activation
         # compute the activation and return the output
         # activation funtion is a step function
@@ -35,27 +35,28 @@ def outNeuron(midThresh):
 # Display results of test
 def display(out, real):
     global inNeuron, inWeight, activation
-    #if out == real:
-    print("[",str(inNeuron[0])," ",str(inNeuron[1]) ,"]",str(out),"should be", str(activation),"true")
-    #else:
-    #    print("[",str(inNeuron[0])," ",str(inNeuron[1]) ,"]",str(out),"should be", str(real),"")
+    if out == real:
+        print("[",str(inNeuron[0])," ",str(inNeuron[1]) ,"]",str(out),"should be **TRUE**", str(activation),"[",str(inWeight[0])," ",str(inWeight[1]) ,"]")
+    else:
+        print("[",str(inNeuron[0])," ",str(inNeuron[1]) ,"]",str(out),"should be", str(real),"[",str(inWeight[0])," ",str(inWeight[1]) ,"]")
 
 while 1:
         # Loop through each lesson in the learning table
         for i in range(len(test)):
                 # Stimulate neurons with test input
-                #inNeuron[0]=test[i][0]
-                #inNeuron[1]=test[i][1]
-                inNeuron[0]=random.uniform(0, 1)
-                inNeuron[1]=random.uniform(0, 1)
+                inNeuron[0]=test[i][0]
+                inNeuron[1]=test[i][1]
+                #inNeuron[0]=random.uniform(0, 1)
+                #inNeuron[1]=random.uniform(0, 1)
                 # Adjust weight of neuron #1
                 # based on feedback, then display
-                out = outNeuron(2)
-                #inWeight[0]+=rate*(test[i][2]-out)
+                out = outNeuron()
+                inWeight[0]+=rate*(test[i][2]-out)
+                inWeight[1]+=rate*(test[i][2]-out)
                 display(out, test[i][2])
                 # Adjust weight of neuron #2
                 # based on feedback, then display
-                #out = outNeuron(2)
+                #out = outNeuron()
                 #inWeight[1]+=rate*(test[i][2]-out)
                 #display(out, test[i][2])
                 # Delay
